5a0ecafff1eefa3f4330b8c2a0bdcbbe5ec89fe6
Guilherme Araújo Sena <127695147+guiasena@users.noreply.github.com>
2024-04-01 14:00:09 -0300

    Update 1.cpp

--- Exercícios/exercicio2/1.cpp
+++ Exercícios/exercicio2/1.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-// Guilherme Araújo Sena || 241008602 || Turma 01
+// Guilherme AraÃºjo Sena || 241008602 || Turma 01
     float x = 7 + 3 * 6 / 2 - 1;
     float y = 2 % 2 + 2 * 2 / 1;
     float z = {3 * 9 * (3 + (9 * 3 / 3))};
     
-    std::cout << "O valor de X é: " << x << std::endl;
-    std::cout << "O valor de Y é: " << y << std::endl;
-    std::cout << "O valor de Z é: " << z << std::endl;
+    cout << "O valor de X Ã©: " << x << endl;
+    cout << "O valor de Y Ã©: " << y << endl;
+    cout << "O valor de Z Ã©: " << z << endl;
 
     return 0;
 }
