66afa7ab5b5305a586fbc0e0301abd0f460657db
Guilherme Araújo Sena <127695147+guiasena@users.noreply.github.com>
2024-04-10 10:50:18 -0300

    Update 1-a.cpp
    add condition to numbers < 0.

--- Exercícios/exercicio4/1-a.cpp
+++ Exercícios/exercicio4/1-a.cpp
@@ -8,7 +8,10 @@ int main() {
 	int number;
 	cout << "insira um numero para o calculo do fatorial: ";
 	cin >> number;
-	
+	if (number <= 0) {
+		cout << "Insira um numero valido! (x > 0) \n";
+		return main();
+	}
 	cout << "O fatorial do numero " << number << " eh " << fatorial(number);
 	return 0;
 }
