ca76caaaf6d1b2eddf6d0161b62b11b823135bfa
Guilherme Araújo Sena <127695147+guiasena@users.noreply.github.com>
2024-07-12 17:55:38 -0300

    Update Questão1.cpp
    Corrigindo

--- Exercícios/exercicio6/Questão1.cpp
+++ Exercícios/exercicio6/Questão1.cpp
@@ -1,31 +1,44 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <sstream>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<sstream>
 using namespace std;
 
-void somar(const string& inputFile, const string& outputFile) {
-
-    ifstream inFile(inputFile);
-    ofstream outFile(outputFile);
-
-    string line;
-
-    while (getline(inFile, line)) {
-        stringstream ss(line);
-        int num;
-        int soma = 0;
-
-        while (ss >> num) {
-            soma += num;
-        }
-        outFile << soma << endl;
-    }
-    inFile.close();
-    outFile.close();
-}
-
 int main() {
-    somar("vetores.txt", "soma.txt");
-    return 0;
+    ifstream arquivoEntrada("vetores.txt");
+    ofstream arquivoSaida("soma.txt");
+
+    if ( arquivoEntrada.is_open() || !arquivoSaida.is_open()) {
+        cerr << "Erro ao abrir ou criar os arquivos." << endl;
+        return 1;
+    }
+
+    vector<int> soma;
+    string linha;
+
+    while (getline(arquivoEntrada, linha)) {
+        istringstream iss(linha);
+        int num;
+        int i = 0;
+
+        while (iss >> num) {
+            if (i >= soma.size()) {
+                soma.push_back(num);
+            } else {
+                soma[i] += num;
+            }
+            i++;
+        }
+    }
+
+    for (int num : soma) {
+        arquivoSaida << num << " ";
+    }
+
+    arquivoEntrada.close();
+    arquivoSaida.close();
+
+    cout << "Processo realizado." << endl;
+
+    return 0;
 }
